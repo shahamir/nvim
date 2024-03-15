c4627eeac850f996ecc4ba587d4f7d58b9e1f3bd
Amir Mottaghi <amirmott@gmail.com>
2024-03-15 23:19:39 +0100

    Adds more plugins
    * Adds plugin for formatting with prettier
    * Adds plugin for lsp
    * Adds plugin for autocomplete

--- lua/amir/lazy.lua
+++ lua/amir/lazy.lua
@@ -11,4 +11,4 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("amir.plugins")
+require("lazy").setup({{import = "amir.plugins"}, {import = "amir.plugins.lsp"}})
---
+++ lua/amir/plugins/init.lua
@@ -0,0 +1,5 @@
+-- add any plugins without custom config here, basically one liners
+
+return {
+  "nvim-lua/plenary.nvim", -- lua functions that many plugins us
+}
